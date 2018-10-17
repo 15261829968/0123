2065307bb3033e03ab1e8325c96b07655c8fe623
1403160123weidayuan <43196101+15261829968@users.noreply.github.com>
2018-10-18 02:06:13 +0800

    第三次上传
    修改了readfile和dosort函数，编写了myMessageOutput函数用于输出排序后的信息，使得作业的要求得以实现，将所有学生的成绩按照不同要求降序排序，但最后输出的txt文件里的内容格式与之前所给的txt不一致，不知道如何解决

--- homework02/homework02/main.cpp
+++ homework02/homework02/main.cpp
@@ -2,9 +2,7 @@
 #include <QVector>
 #include <QTextStream>
 #include <QFile>
-using namespace std;
 
-#define n 4
 
 namespace SK {
 enum SortKind{
@@ -44,15 +42,15 @@ enum SortKind{
 }
 
 typedef struct{
-    QList<QString>student;
+    QStringList student;//定义一个存放信息的student字符串
     // 请补全结构定义
 } studData;
 
 QDebug operator<< (QDebug d, const studData &data) {
     int i;
     for(i=0;i<data.student.size();i++)
-        d<<data.student.at(i);
-        return d;
+        d.noquote().nospace()<<QString(data.student.at(i))<<"\t" ;
+    return d;
     // 请补全运算符重载函数，使其可以直接输出studData结构
 
 }
@@ -65,16 +63,47 @@ public:
 private:
     int currentColumn;
 };
+#define compare(a)   (d1.student.at(a)>=d2.student.at(a))?  1:0
 
 bool myCmp::operator()(const studData &d1, const studData &d2)
 {
     bool result = false;
     quint32 sortedColumn = 0x00000001<<currentColumn;
     switch (sortedColumn)
-    {
-    default:;break; case SK::col01:
-         result=d1.student.at(currentColumn)>d2.student.at(currentColumn);
-         break;
+        {
+           case SK::col01:result=compare(1);break;
+           case SK::col02:result=compare(2);break;
+           case SK::col03:result=compare(3);break;
+           case SK::col04:result=compare(4);break;
+           case SK::col05:result=compare(5);break;
+           case SK::col06:result=compare(6);break;
+           case SK::col07:result=compare(7);break;
+           case SK::col08:result=compare(8);break;
+           case SK::col09:result=compare(9);break;
+           case SK::col10:result=compare(10);break;
+           case SK::col11:result=compare(11);break;
+           case SK::col12:result=compare(12);break;
+           case SK::col13:result=compare(13);break;
+           case SK::col14:result=compare(14);break;
+           case SK::col15:result=compare(15);break;
+           case SK::col16:result=compare(16);break;
+           case SK::col17:result=compare(17);break;
+           case SK::col18:result=compare(18);break;
+           case SK::col19:result=compare(19);break;
+           case SK::col20:result=compare(20);break;
+           case SK::col21:result=compare(21);break;
+           case SK::col22:result=compare(22);break;
+           case SK::col23:result=compare(23);break;
+           case SK::col24:result=compare(24);break;
+           case SK::col25:result=compare(25);break;
+           case SK::col26:result=compare(26);break;
+           case SK::col27:result=compare(27);break;
+           case SK::col28:result=compare(28);break;
+           case SK::col29:result=compare(29);break;
+           case SK::col30:result=compare(30);break;
+           case SK::col31:result=compare(31);break;
+           case SK::col32:result=compare(32);break;
+           default:;break;
         }
         return result;
 
@@ -82,51 +111,91 @@ bool myCmp::operator()(const studData &d1, const studData &d2)
 
 }
 
-
+//排序类
 class ScoreSorter
 {
 public:
     ScoreSorter(QString dataFile);
-    void readFile();
-    void doSort();
+    void readFile();//读文本
+    void doSort();//执行排序
 private:
-    QString Sorter;
-    QList<studData> Student;
-    QByteArray line;   //数据表头
+    QString pathfile;
+    QList<studData> data;
+    studData title;//数据表头
+
 };
 // 请补全
 ScoreSorter::ScoreSorter(QString dataFile)
 {
-    Sorter=dataFile;
+   pathfile=dataFile;
 }
-void ScoreSorter::doSort(){
-    myCmp compare(n);
-    qDebug()<<"排序后输出，当前排序第"<<n<<"列";
-    std::sort(Student.begin(),Student.end(),compare);
-    qDebug()<<Student;
-    qDebug()<<"-----------------------------------"<<endl;
+void ScoreSorter::doSort()//排序函数
+{
+    for(int i=1;i<title.student.size();i++)
+    {
+        myCmp stducmp(i-1);    //初始化排序规则对象
+        std::sort(data.begin() , data.end() , stducmp );  //排序
+
+        qDebug()<<"排序后输出，当前排序第 "<<i+1 <<" 列：";
+        qDebug() << '\t'<< (title);    //输出表头
+
+        for(int i=0;i<data.size();i++)
+            qDebug() << data.at(i);//qDebug重载输出
+        qDebug()<<"-----------------------------------------------------------------------------------------------------------\n";
+
+    }
 }
+
+
+
 void ScoreSorter::readFile(){
-    QFile file(Sorter);
+    QFile file(pathfile);
     if(!file.open(QIODevice::ReadOnly | QIODevice::Text)){
         qDebug()<<QString("文件打开失败");
         return;
     }
-    while(!file.atEnd()){
-        studData tempstudent;
-        this->line=file.readLine();
-        QString string(line);
-      tempstudent.student=string.split(" ",QString::SkipEmptyParts);
-       Student.append(tempstudent);
-    }
-    file.close();
-}
+     QString TITLE(file.readLine());
+    title.student = TITLE.split(" ", QString::SkipEmptyParts);
+     if((title.student).last() == "\n")
+         title.student.removeLast();
+         studData filedata;//TXT中数据
 
-//Void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg){}
+         while(!file.atEnd())
+             {
+                 QByteArray line = file.readLine();
+                 QString str(line);
+
+                 filedata.student = str.split(" ", QString::SkipEmptyParts);//读取成绩数据
+
+                 if((filedata.student).last() == "\n")  //去末尾'\n'
+                     filedata.student.removeLast();
+                 if(filedata.student.size()==0) continue;
+                 data.append(filedata);//添加数据到data
+             }
+             file.close();
+         }
+
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    // 输出信息至文件中（读写、追加形式）
+
+    QFile file("Sorter_data.txt");
+    file.open(QIODevice::WriteOnly | QIODevice::Append);
+    QTextStream stream(&file);
+    stream <<msg << endl;
+    file.flush();
+    file.close();
+    QTextStream ts (stdout);
+    ts << msg << endl;
+    file.flush();
+    file.close();
+
+    // 自定义qDebug
+}
 
 int main()
 {
-    //qInstallMessageHandler(myMessageOutput);
+   qInstallMessageHandler(myMessageOutput);
     QString datafile = "data.txt";
 
     // 如果排序后文件已存在，则删除之
